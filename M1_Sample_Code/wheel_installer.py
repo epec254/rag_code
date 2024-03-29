23b3251a203893117e21722005e1ebff95a7e091
Eric Peter <epeter@gmail.com>
2024-03-28 17:46:17 -0700

    Make pip verbose for debugging

--- M1_Sample_Code/wheel_installer.py
+++ M1_Sample_Code/wheel_installer.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-# MAGIC %pip install --quiet "PUT_RAG_STUDIO_WHEEL_HERE"
-# MAGIC %pip install --quiet "PUT_RAG_EVAL_SUITE_WHEEL_HERE"
+# MAGIC %pip install "PUT_RAG_STUDIO_WHEEL_HERE"
+# MAGIC %pip install "PUT_RAG_EVAL_SUITE_WHEEL_HERE"
